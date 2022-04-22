7f05ffec9fa6c33ba162ece00082638024b7b96a
Vivian Azevedo <viviancrisga@gmail.com>
2022-04-22 23:25:56 +1000

    Initial commit

---
+++ utils.js
@@ -0,0 +1,6 @@
+function printSmokeOnTheWater() {
+  document.write("We all came out to MONTREAUX <br>");
+  document.write("On the Lake Geneva shoreline <br>");
+  document.write("To make records with a mobile <br>");
+  document.write("We didn't have much time <br>");
+}
